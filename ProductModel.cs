853345f044352c9b814b5443a23693f078edca6f
Hugo Aparecido Gregorio de Almeida <120059026+HugoAparecido@users.noreply.github.com>
2024-11-27 20:45:43 -0300

    Adicionar arquivos de projeto.

---
+++ ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransformarJSON
+{
+    public class ProductModel
+    {
+        public string produto { get; set; }
+        public float preco { get; set; }
+        public int quantidade { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using TransformarJSON;
+
+// Convertendo Json para um objeto
+var json = "{\"produto\":\"tenis\",\"preco\":150.00,\"quantidade\":2}";
+var jsonObject = JsonConvert.DeserializeObject<ProductModel>(json);
+var ObjectToJson = JsonConvert.SerializeObject(jsonObject);
+Console.WriteLine(ObjectToJson);
